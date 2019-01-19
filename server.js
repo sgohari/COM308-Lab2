e40f1f92733be222b1492b57f28e5cb8c1e80791
sgohary <nasir_2005@msn.com>
2019-01-19 17:04:34 -0500

    name and details added

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Syed Nasir Gohary \n300937424\n');
+    res.end('Baby steps towards NodeJs\n\tSyed Nasir Gohary\n\t\t300937424\n\n\t\t\tSee You Next Saturday');
+  
   });
   
   server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
   });
+  
